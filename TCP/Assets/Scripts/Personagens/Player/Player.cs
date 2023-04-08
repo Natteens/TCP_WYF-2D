4a0a408ca826a2b6a189eac159855ba601e91525
Natte <107698186+Natteens@users.noreply.github.com>
2023-04-07 23:23:00 -0300

    Versão Base do jogo - update 0
    implementei algumas mecanicas simples, o personagem se movimenta usando WASD e corre usando o SHIFT. o personagem que estou usando aqui e so um personagem para testes q peguei de outro projeto meu.

---
+++ TCP/Assets/Scripts/Personagens/Player/Player.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+{
+
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
+ 
+
+    private Rigidbody2D rig;
+
+    private float initialSpeed;
+    private bool _isRunning;
+ 
+    private Vector2 _direction;
+
+    public Vector2 direction
+    {
+        get { return _direction; }
+        set { _direction = value; }
+    }
+    public bool isRunning
+    {
+        get { return _isRunning; }
+        set { _isRunning = value; }
+    }
+
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
+
+    }
+
+    private void Update()
+    {
+        OnInput();
+        OnRun();
+       
+    }
+
+    private void FixedUpdate()
+    {
+        OnMove();
+    }
+
+    #region Movement
+
+    void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void OnMove()
+    {
+       
+            rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);   
+    }
+
+    void OnRun()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            _isRunning = true;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+            _isRunning = false;
+        }
+    }
+
+  
+
+
+
+
+    #endregion
+}
